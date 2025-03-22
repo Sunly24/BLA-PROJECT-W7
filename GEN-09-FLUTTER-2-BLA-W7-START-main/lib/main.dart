c571d1935295e39662ea818f03ba95dee00b1ebb
Sunly <sonlychao169@gmail.com>
2025-03-22 21:17:13 +0700

    BLA-203– Refactor your project structure

--- GEN-09-FLUTTER-2-BLA-W7-START-main/lib/main.dart
+++ GEN-09-FLUTTER-2-BLA-W7-START-main/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'repository/mock/mock_locations_repository.dart';
-import 'repository/mock/mock_rides_repository.dart';
+import 'data/repository/mock/mock_locations_repository.dart';
+import 'data/repository/mock/mock_rides_repository.dart';
 import 'service/locations_service.dart';
 import 'service/rides_service.dart';
 
-import 'repository/mock/mock_ride_preferences_repository.dart';
+import 'data/repository/mock/mock_ride_preferences_repository.dart';
 import 'ui/screens/ride_pref/ride_pref_screen.dart';
 import 'ui/providers/ride_prefs_provider.dart';
 import 'ui/theme/theme.dart';
