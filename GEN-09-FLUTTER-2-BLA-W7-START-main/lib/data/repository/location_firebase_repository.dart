b050fcce0b63ae7e68307e8affeac0c5f35dfca5
Sunly <sonlychao169@gmail.com>
2025-03-25 23:31:27 +0700

    BLA-205 – BONUS - Connecting to Firebase Realtime Database via REST API - In Progress

---
+++ GEN-09-FLUTTER-2-BLA-W7-START-main/lib/data/repository/location_firebase_repository.dart
@@ -0,0 +1,10 @@
+import '/model/location/locations.dart';
+
+import 'locations_repository.dart';
+
+class LocationFirebaseRepository extends LocationsRepository {
+  @override
+  List<Location> getLocations() {
+    throw UnimplementedError();
+  }
+}
--- GEN-09-FLUTTER-2-BLA-W7-START-main/lib/ui/widgets/errors/display/bla_divider.dart
+++ GEN-09-FLUTTER-2-BLA-W7-START-main/lib/ui/widgets/errors/display/bla_divider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../theme/theme.dart';
+import '../../../theme/theme.dart';
 
 class BlaDivider extends StatelessWidget {
   const BlaDivider({super.key});
